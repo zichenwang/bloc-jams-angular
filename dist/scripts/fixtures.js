aa01c3db9f8f19d5805c0695de7af690c823e518
Zichen Wang <wangzichen530@hotmail.com>
2016-11-10 14:32:51 -0600

    checkpoint 5

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,116 @@
+var albumImaginDragons = {
+    title: 'Night Visions',
+    artist: 'Imagine Dragons',
+    label: 'Alternative',
+    year: '2012',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [
+        {
+            title: 'It\'s time',
+            duration: '4:00'
+        },
+        {
+            title: 'Demons',
+            duration: '2:58'
+        },
+        {
+            title: 'On Top of the World',
+            duration: '3:12'
+        },
+        {
+            title: 'Bleeding Out',
+            duration: '3:43'
+        },
+        {
+            title: 'UnderDog',
+            duration: '3:29'
+        }
+     ]
+};
+
+var albumTFK = {
+    title: 'Oxygen: Inhale',
+    artist: 'Thousand Foot Krutch',
+    label: 'Rock',
+    year: '2014',
+    albumArtUrl: 'assets/images/album_covers/04.png',
+    songs: [
+        {
+            title: 'Like a Machine',
+            duration: '3:44'
+        },
+        {
+            title: 'Untraveled Road',
+            duration: '3:56'
+        },
+        {
+            title: 'Born This Way',
+            duration: '3:26'
+        }
+     ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+            title: 'Hello, Operator?',
+            duration: '1:01'
+        },
+        {
+            title: 'Ring, ring, ring',
+            duration: '5:01'
+        },
+        {
+            title: 'Fits in your pocket',
+            duration: '3:21'
+        },
+        {
+            title: 'Can you hear me now?',
+            duration: '3:14'
+        },
+        {
+            title: 'Wrong phone number',
+            duration: '2:15'
+        }
+     ]
+};
+
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {
+            title: 'Blue',
+            duration: 161.71,
+            audioUrl: 'assets/music/blue'
+        },
+        {
+            title: 'Green',
+            duration: 103.96,
+            audioUrl: 'assets/music/green'
+        },
+        {
+            title: 'Red',
+            duration: 268.45,
+            audioUrl: 'assets/music/red'
+        },
+        {
+            title: 'Pink',
+            duration: 153.14,
+            audioUrl: 'assets/music/pink'
+        },
+        {
+            title: 'Magenta',
+            duration: 374.22,
+            audioUrl: 'assets/music/magenta'
+        }
+     ]
+};
